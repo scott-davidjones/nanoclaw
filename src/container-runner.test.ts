60a40bc7b8e9154e286a7e42d9d9e45855b1ecbb
Scott-David Jones <scott@in-line.studio>
2026-04-07 09:45:40 +0000

    fix: add MCP_MEMORY_URL to container-runner test mock

--- src/container-runner.test.ts
+++ src/container-runner.test.ts
@@ -15,6 +15,7 @@ vi.mock('./config.js', () => ({
   GROUPS_DIR: '/tmp/nanoclaw-test-groups',
   IDLE_TIMEOUT: 1800000, // 30min
   ONECLI_URL: 'http://localhost:10254',
+  MCP_MEMORY_URL: '',
   TIMEZONE: 'America/Los_Angeles',
 }));
 
